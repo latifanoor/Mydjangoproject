b733d1fedc8a282cf4f0befb62228d66e5a08638
latifanoor <latiffa.noor3@gmail.com>
2018-05-23 12:10:55 +0300

    djangoproject

--- swahilipot/admin.py
+++ swahilipot/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Members,Board,Topic,Post
+admin.site.register(Members)
+admin.site.register(Board)
+admin.site.register(Topic)
+admin.site.register(Post)
